1b912fbcfcdbd491846a9d8652d912aeb07e4363
Donghun <2dh2@naver.com>
2025-04-30 13:31:11 +0900

    feat. /introduce GET API 구현

--- src/main/java/com/example/bcsd/controller/IntroduceController.java
+++ src/main/java/com/example/bcsd/controller/IntroduceController.java
@@ -1,4 +1,14 @@
 package com.example.bcsd.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/introduce")
 public class IntroduceController {
+    @GetMapping
+    public String getIntroduce() {
+        return "안녕하세요 제 이름은 이동훈입니다!";
+    }
 }
