d704ab89eb068b367560e14c92ce61454ae9c88b
Donghun <2dh2@naver.com>
2025-04-30 13:41:23 +0900

    feat. name 쿼리 파라미터 추가

--- src/main/java/com/example/bcsd/controller/IntroduceController.java
+++ src/main/java/com/example/bcsd/controller/IntroduceController.java
@@ -2,13 +2,16 @@ package com.example.bcsd.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/introduce")
 public class IntroduceController {
     @GetMapping
-    public String getIntroduce() {
-        return "안녕하세요 제 이름은 이동훈입니다!";
+    public String getIntroduce(
+            @RequestParam(name = "name", defaultValue="이동훈", required = false) String name
+    ) {
+        return "안녕하세요 제 이름은 " + name + "입니다!";
     }
 }
