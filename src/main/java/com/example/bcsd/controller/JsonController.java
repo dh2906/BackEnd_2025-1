6e88df3319e3029e8461eaf259617a8393def8a2
Donghun <2dh2@naver.com>
2025-04-30 13:50:12 +0900

    feat. /json GET API 구현

---
+++ src/main/java/com/example/bcsd/controller/JsonController.java
@@ -0,0 +1,18 @@
+package com.example.bcsd.controller;
+
+import com.example.bcsd.controller.dto.JsonResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/json")
+public class JsonController {
+    @GetMapping
+    public ResponseEntity<JsonResponse> getJson() {
+        JsonResponse response = new JsonResponse(24, "이동훈");
+
+        return ResponseEntity.ok(response);
+    }
+}
---
+++ src/main/java/com/example/bcsd/controller/dto/JsonResponse.java
@@ -0,0 +1,19 @@
+package com.example.bcsd.controller.dto;
+
+public class JsonResponse {
+    private int age;
+    private String name;
+
+    public JsonResponse(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
