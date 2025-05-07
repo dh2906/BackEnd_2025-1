f8f98c8508abeb37f38186a9f338ab37364a0bd7
Donghun <2dh2@naver.com>
2025-05-07 17:12:48 +0900

    fix. /introduce 뷰 반환되도록 수정

--- src/main/java/com/example/bcsd/controller/IntroduceController.java
+++ src/main/java/com/example/bcsd/controller/IntroduceController.java
@@ -1,17 +1,22 @@
 package com.example.bcsd.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/introduce")
 public class IntroduceController {
     @GetMapping
     public String getIntroduce(
-            @RequestParam(name = "name", defaultValue="이동훈", required = false) String name
+            @RequestParam(name = "name", defaultValue="이동훈", required = false) String name,
+            Model model
     ) {
-        return "안녕하세요 제 이름은 " + name + "입니다!";
+        model.addAttribute("name" , name);
+
+        return "hello";
     }
 }
