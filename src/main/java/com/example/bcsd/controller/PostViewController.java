4a86d9f0bf3ff32e6a38b409a6c80c8396173763
Donghun <2dh2@naver.com>
2025-05-08 14:25:45 +0900

    feat. 모든 article을 보여주는 뷰 구현

--- src/main/java/com/example/bcsd/controller/PostViewController.java
+++ src/main/java/com/example/bcsd/controller/PostViewController.java
@@ -1,4 +1,29 @@
 package com.example.bcsd.controller;
 
+import com.example.bcsd.model.Article;
+import com.example.bcsd.service.ArticleService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/posts")
 public class PostViewController {
+    private final ArticleService articleService;
+
+    public PostViewController(ArticleService articleService) {
+        this.articleService = articleService;
+    }
+
+    @GetMapping
+    public String getAllArticlesView(Model model) {
+        List<Article> articles = articleService.getAllArticles();
+
+        model.addAttribute("articleList", articles);
+
+        return "post";
+    }
 }
