417bfe13f8787e4e2ae252c4e380b23a55291c26
Donghun <2dh2@naver.com>
2025-05-11 18:35:11 +0900

    refactory. 예외 발생 시 에러 메세지 로깅 처리

--- src/main/java/com/example/bcsd/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/bcsd/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.example.bcsd.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -7,9 +8,11 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.util.NoSuchElementException;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
     @ExceptionHandler(NoSuchElementException.class)
-    public ResponseEntity<Void> notFoundExceptionHandle() {
+    public ResponseEntity<Void> notFoundExceptionHandle(NoSuchElementException ex) {
+        log.error(ex.getMessage());
         return ResponseEntity.notFound().build();
     }
 
