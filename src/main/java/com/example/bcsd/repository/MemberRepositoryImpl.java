5ef82850fd89f950ca40d47ee03dfefbba2f6f99
Donghun <2dh2@naver.com>
2025-05-12 15:35:30 +0900

    fix. MemberRepository 구현체로 지정

--- src/main/java/com/example/bcsd/repository/MemberRepositoryImpl.java
+++ src/main/java/com/example/bcsd/repository/MemberRepositoryImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 import java.util.*;
 
 @Repository
-public class MemberRepositoryImpl {
+public class MemberRepositoryImpl implements MemberRepository {
     private Long key = 1L;
     private final Map<Long, Member> members = new HashMap<>();
 
