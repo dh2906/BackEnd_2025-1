e4f7aca559367ca6d31358f44ca15dbe0c37cf1d
Donghun <2dh2@naver.com>
2025-05-14 20:16:16 +0900

    feat. 멤버 Repository 구현

--- src/main/java/com/example/bcsd/repository/MemberJDBCRepositoryImpl.java
+++ src/main/java/com/example/bcsd/repository/MemberJDBCRepositoryImpl.java
@@ -1,4 +1,82 @@
 package com.example.bcsd.repository;
 
-public class MemberJDBCRepositoryImpl {
+import com.example.bcsd.model.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberJDBCRepositoryImpl implements MemberRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Override
+    public List<Member> findAll() {
+        String sql = "SELECT * FROM Member";
+
+        return jdbcTemplate.query(sql, memberRowMapper());
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        String sql = "SELECT * FROM Member WHERE id = ?";
+
+        return Optional.ofNullable(
+                jdbcTemplate.queryForObject(
+                        sql,
+                        memberRowMapper(),
+                        id
+                )
+        );
+    }
+
+    @Override
+    public Member save(Member member) {
+        String sql = "INSERT INTO Member (name, email, password) VALUES (?, ?, ?)";
+
+        jdbcTemplate.update(
+                sql,
+                member.getName(),
+                member.getEmail(),
+                member.getPassword()
+        );
+
+        return member;
+    }
+
+    @Override
+    public Member save(Long id, Member member) {
+        String sql = "UPDATE Member SET name = ?, email = ? password = ?, WHERE id = ?";
+
+        jdbcTemplate.update(
+                sql,
+                member.getName(),
+                member.getEmail(),
+                member.getPassword(),
+                id
+        );
+
+        return member;
+    }
+
+    @Override
+    public void delete(Long id) {
+        String sql = "DELETE from Member WHERE id = ?";
+
+        jdbcTemplate.update(sql, id);
+    }
+
+    private RowMapper<Member> memberRowMapper() {
+        return ((rs, rowNum) -> {
+            return Member.builder()
+                         .name(rs.getString("name"))
+                         .email(rs.getString("email"))
+                         .password(rs.getString("password"))
+                         .build();
+        });
+    }
 }
