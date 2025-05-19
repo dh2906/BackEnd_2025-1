21779600c4a789847e71d9bc6f3952383e83a4bf
Donghun <2dh2@naver.com>
2025-05-19 14:11:04 +0900

    feat. 멤버 비밀번호 암호화

--- src/main/java/com/example/bcsd/dto/request/MemberRequest.java
+++ src/main/java/com/example/bcsd/dto/request/MemberRequest.java
@@ -1,12 +1,18 @@
 package com.example.bcsd.dto.request;
 
 import com.example.bcsd.model.Member;
+import org.mindrot.jbcrypt.BCrypt;
 
 public record MemberRequest(
         String name,
         String email,
         String password
 ) {
+    public MemberRequest(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = BCrypt.hashpw(password, BCrypt.gensalt());
+    }
     public Member toEntity() {
         return Member.builder()
                 .name(name)
