f722633b56edfe04aea89bbf4d0488bdfd3639dd
Donghun <2dh2@naver.com>
2025-05-24 10:17:47 +0900

    refactory. 게시글 유효성 검증 어노테이션에 에러 메세지 추가

--- src/main/java/com/example/bcsd/dto/request/ArticleUpdateRequest.java
+++ src/main/java/com/example/bcsd/dto/request/ArticleUpdateRequest.java
@@ -5,16 +5,16 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record ArticleUpdateRequest (
-        @NotNull
+        @NotNull(message = "작성자 ID가 누락되었습니다.")
         Long authorId,
 
-        @NotNull
+        @NotNull(message = "게시판 ID가 누락되었습니다.")
         Long boardId,
 
-        @NotBlank
-        @Size(max=255)
+        @NotBlank(message = "제목이 누락되었습니다.")
+        @Size(max=255, message = "제목의 최대 길이를 벗어났습니다. (최대 길이 : 255자)")
         String title,
 
-        @NotBlank
+        @NotBlank(message = "본문이 누락되었습니다.")
         String content
 ) { }
