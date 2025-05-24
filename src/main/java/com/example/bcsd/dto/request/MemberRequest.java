eefc560709e75def82c5dde7a5c0cbdebb1f789d
Donghun <2dh2@naver.com>
2025-05-24 11:56:12 +0900

    feat. Member 관련 클래스에 유효성 검증 어노테이션 추가

--- src/main/java/com/example/bcsd/dto/request/MemberRequest.java
+++ src/main/java/com/example/bcsd/dto/request/MemberRequest.java
@@ -3,12 +3,21 @@ package com.example.bcsd.dto.request;
 import com.example.bcsd.dto.deserializer.PasswordBcryptDeserializer;
 import com.example.bcsd.model.Member;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record MemberRequest(
+        @NotNull(message = "이름이 누락되었습니다.")
+        @Size(max = 100, message = "이름의 최대 길이를 벗어났습니다. (최대 길이 : 100자)")
         String name,
+
+        @NotNull(message = "이메일이 누락되었습니다.")
+        @Size(max = 100, message = "이메일의 최대 길이를 벗어났습니다. (최대 길이 : 100자)")
         String email,
 
-        @JsonDeserialize(using = PasswordBcryptDeserializer.class )
+        @NotNull(message = "비밀번호가 누락되었습니다.")
+        @Size(max = 255, message = "비밀번호의 최대 길이를 벗어났습니다. (최대 길이 : 100자)")
+        @JsonDeserialize(using = PasswordBcryptDeserializer.class)
         String password
 ) {
     public MemberRequest(String name, String email, String password) {
