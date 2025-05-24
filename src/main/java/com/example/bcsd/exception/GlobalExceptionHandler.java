42ca928b8097589723ffef42b52e46fe9b506cd6
Donghun <2dh2@naver.com>
2025-05-24 12:05:45 +0900

    feat. valid 어노테이션으로 발생하는 예외 처리

--- src/main/java/com/example/bcsd/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/bcsd/exception/GlobalExceptionHandler.java
@@ -1,21 +1,40 @@
 package com.example.bcsd.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<String> ExceptionHandle(CustomException ex) {
-        log.error(ex.getMessage());
+        log.error("[Exception] : " + ex.getMessage());
 
         return ResponseEntity.status(
                 ex.getExceptionMessage()
                         .getStatus()
                 )
                 .body(ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<String>> ExceptionHandle(MethodArgumentNotValidException ex) {
+        List<String> errorMessages = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList());
+
+        log.error("[Exception] : " + errorMessages.toString());
+
+
+        return ResponseEntity.badRequest().body(errorMessages);
     }
 }
