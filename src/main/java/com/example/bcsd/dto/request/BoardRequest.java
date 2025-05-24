6dc53a63914e4892cc74ca28198ba9828dd3318b
Donghun <2dh2@naver.com>
2025-05-24 11:56:00 +0900

    feat. Board 관련 클래스에 유효성 검증 어노테이션 추가

--- src/main/java/com/example/bcsd/dto/request/BoardRequest.java
+++ src/main/java/com/example/bcsd/dto/request/BoardRequest.java
@@ -1,8 +1,12 @@
 package com.example.bcsd.dto.request;
 
 import com.example.bcsd.model.Board;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record BoardRequest(
+        @NotNull(message = "게시판 이름이 누락되었습니다.")
+        @Size(max = 100, message = "게시판 이름의 최대 길이를 벗어났습니다. (최대 길이 : 100자)")
         String name
 ) {
     public Board toEntity() {
