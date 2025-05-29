16912f4aedeb3829f7be17a83a2bddf371b1e0ed
Donghun <2dh2@naver.com>
2025-05-29 14:25:18 +0900

    feat. 게시물 JPA Repository 구현

--- src/main/java/com/example/bcsd/repository/jpa/ArticleJPARepositoryImpl.java
+++ src/main/java/com/example/bcsd/repository/jpa/ArticleJPARepositoryImpl.java
@@ -2,38 +2,54 @@ package com.example.bcsd.repository.jpa;
 
 import com.example.bcsd.model.Article;
 import com.example.bcsd.repository.ArticleRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class ArticleJPARepositoryImpl implements ArticleRepository {
+    @PersistenceContext
+    private EntityManager entityManager;
+
     @Override
     public List<Article> findAllByBoardId(Long boardId) {
-        return List.of();
+        return entityManager
+                .createQuery("SELECT a FROM Article a WHERE boardId = :boardId", Article.class)
+                .setParameter("boardId", boardId)
+                .getResultList();
     }
 
     @Override
     public List<Article> findAll() {
-        return List.of();
+        return entityManager
+                .createQuery("SELECT a FROM Article a", Article.class)
+                .getResultList();
     }
 
     @Override
-    public Optional<Article> findById(Long aLong) {
-        return Optional.empty();
+    public Optional<Article> findById(Long id) {
+        return Optional.ofNullable(entityManager.find(Article.class, id));
     }
 
     @Override
     public Article save(Article entity) {
+        entityManager.persist(entity);
+
+        return entity;
+    }
+
+    @Override
+    public Article save(Long id, Article entity) {
         return null;
     }
 
     @Override
-    public Article save(Long aLong, Article entity) {
-        return null;
-    }
+    public void delete(Long id) {
+        Article article = entityManager.find(Article.class, id);
 
-    @Override
-    public void delete(Long aLong) {
-
+        entityManager.remove(id);
     }
 }
