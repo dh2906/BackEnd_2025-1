9ab2b2dff538863c7a1efe3c8180cfb9ed717f4b
Donghun <2dh2@naver.com>
2025-05-29 14:25:46 +0900

    feat. 멤버 JPA Repository 구현

--- src/main/java/com/example/bcsd/repository/jpa/MemberJPARepositoryImpl.java
+++ src/main/java/com/example/bcsd/repository/jpa/MemberJPARepositoryImpl.java
@@ -2,33 +2,44 @@ package com.example.bcsd.repository.jpa;
 
 import com.example.bcsd.model.Member;
 import com.example.bcsd.repository.MemberRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class MemberJPARepositoryImpl implements MemberRepository {
+    @PersistenceContext
+    private EntityManager entityManager;
+
     @Override
     public List<Member> findAll() {
-        return List.of();
+        return entityManager
+                .createQuery("SELECT m FROM Member m", Member.class)
+                .getResultList();
     }
 
     @Override
-    public Optional<Member> findById(Long aLong) {
-        return Optional.empty();
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(entityManager.find(Member.class, id));
     }
 
     @Override
     public Member save(Member entity) {
+        entityManager.persist(entity);
+
+        return entity;
+    }
+
+    @Override
+    public Member save(Long id, Member entity) {
         return null;
     }
 
     @Override
-    public Member save(Long aLong, Member entity) {
-        return null;
-    }
-
-    @Override
-    public void delete(Long aLong) {
-
+    public void delete(Long id) {
+        entityManager.remove(id);
     }
 }
