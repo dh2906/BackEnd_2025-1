327a6edd86046348001c59e1aaa481a1213fb48b
Donghun <2dh2@naver.com>
2025-05-29 14:25:32 +0900

    feat. 게시판 JPA Repository 구현

--- src/main/java/com/example/bcsd/repository/jpa/BoardJPARepositoryImpl.java
+++ src/main/java/com/example/bcsd/repository/jpa/BoardJPARepositoryImpl.java
@@ -2,33 +2,46 @@ package com.example.bcsd.repository.jpa;
 
 import com.example.bcsd.model.Board;
 import com.example.bcsd.repository.BoardRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class BoardJPARepositoryImpl implements BoardRepository {
+    @PersistenceContext
+    private EntityManager entityManager;
+
     @Override
     public List<Board> findAll() {
-        return List.of();
+        return entityManager
+                .createQuery("SELECT b FROM Board b", Board.class)
+                .getResultList();
     }
 
     @Override
-    public Optional<Board> findById(Long aLong) {
-        return Optional.empty();
+    public Optional<Board> findById(Long id) {
+        return Optional.ofNullable(entityManager.find(Board.class, id));
     }
 
     @Override
     public Board save(Board entity) {
+        entityManager.persist(entity);
+
+        return entity;
+    }
+
+    @Override
+    public Board save(Long id, Board entity) {
         return null;
     }
 
     @Override
-    public Board save(Long aLong, Board entity) {
-        return null;
-    }
+    public void delete(Long id) {
+        Board board = entityManager.find(Board.class, id);
 
-    @Override
-    public void delete(Long aLong) {
-
+        entityManager.remove(id);
     }
 }
